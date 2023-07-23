7e12e46e7a477a88e338fb6522fc798472bae210
unknown <satya-1298>
2023-07-23 16:55:52 +0530

    [Satya]Add : Creating a Data Table

---
+++ DataTable_Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTable_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            while (true)
+            {
+                Console.WriteLine("1-Data Table");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option) 
+                {
+                    case 1:
+                        Tables_Data tables_Data = new Tables_Data();
+                        tables_Data.Table();
+                        break;
+                }
+            }
+           
+        }
+       
+    }
+}
---
+++ DataTable_Demo/Tables_Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTable_Demo
+{
+    public class Tables_Data
+    {
+        public void Table()
+        {
+
+            DataTable dataTable = new DataTable("Test");
+            dataTable.Columns.Add("ProductID", typeof(int));
+            dataTable.Columns.Add("UserID");
+            dataTable.Columns.Add("Rating");
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("IsLike");
+
+            dataTable.Rows.Add("1", "1", "6", "Nice", "true");
+            dataTable.Rows.Add("2", "2", "4", "Average", "false");
+           
+        }
+    }
+}
