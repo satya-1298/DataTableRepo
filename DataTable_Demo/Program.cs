a588186b6f53484a041ac2d08149e6009f098c6a
unknown <satya-1298>
2023-07-23 18:42:41 +0530

    [Satya]Add : Converting the DataTable to the List

--- DataTable_Demo/Program.cs
+++ DataTable_Demo/Program.cs
@@ -11,19 +11,27 @@ namespace DataTable_Demo
     {
         static void Main(string[] args)
         {
-           
+            DataTable table = new DataTable("Table_Data");
+            Tables_Data tables_Data = new Tables_Data();
             while (true)
             {
                 Console.WriteLine("-------------------------------------");
                 Console.WriteLine("1-Data Table");
+                Console.WriteLine("2-Display ");
+                Console.WriteLine("3-Table to List");
                 Console.WriteLine("Choose the above option");
                 Console.WriteLine("-------------------------------------");
                 int option =Convert.ToInt32(Console.ReadLine());
                 switch(option) 
                 {
                     case 1:
-                        Tables_Data tables_Data = new Tables_Data();
                         tables_Data.Table();
+                        break;
+                    case 2:
+                        tables_Data.Display(); 
+                        break;
+                    case 3:
+                        tables_Data.Converting_Table_List();
                         break;
                 }
             }
--- DataTable_Demo/Tables_Data.cs
+++ DataTable_Demo/Tables_Data.cs
@@ -4,15 +4,16 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace DataTable_Demo
 {
     public class Tables_Data
     {
+        DataTable dataTable = new DataTable("Employee");
         public void Table()
         {
 
-            DataTable dataTable = new DataTable("Test");
             dataTable.Columns.Add("ProductID", typeof(int));
             dataTable.Columns.Add("UserID");
             dataTable.Columns.Add("Rating");
@@ -22,11 +23,15 @@ namespace DataTable_Demo
             dataTable.Rows.Add("1", "1", "6", "Nice", "true");
             dataTable.Rows.Add("2", "2", "4", "Average", "false");
             //Printing all the data in table
+        }
+        public void Display()
+        { 
             Console.WriteLine("------------------------------------");
             foreach (DataRow row in dataTable.Rows)
             {
                 Console.WriteLine("ProductID ={0} , UserID = {1} , Rating = {2} , Review = {3} ,IsLike = {4} ", row["ProductID"], row["UserID"], row["Rating"], row["Review"], row["IsLike"]);
             }
+
             //Display certain Column
             Console.WriteLine("-------------------------------------");
             foreach (DataRow rows in dataTable.Rows)
@@ -48,8 +53,40 @@ namespace DataTable_Demo
             Console.WriteLine("No .of Rows "+dataTable.Rows.Count);
             Console.WriteLine("-------------------------------------");
             Console.WriteLine("No .of Columns " + dataTable.Columns.Count);
-
+           // dataTable=
 
         }
+        public void Converting_Table_List()
+        {
+            // Table(); 
+            Console.WriteLine("Converting table to link");
+            Console.WriteLine("-------------------------------------");
+            List<Employee> list= new List<Employee>();  
+            for(int val=0;val<dataTable.Rows.Count;val++) 
+            {
+                Employee employee=new Employee();
+                employee.ProductID = Convert.ToInt32(dataTable.Rows[val]["ProductID"]);
+                employee.UserID = Convert.ToInt32(dataTable.Rows[val]["UserID"]);
+                employee.Rating= Convert.ToInt32(dataTable.Rows[val]["Rating"]);
+                employee.Review = dataTable.Rows[val]["Review"].ToString();
+                employee.IsLike = Convert.ToBoolean(dataTable.Rows[val]["IsLike"]);
+                list.Add(employee);
+            }
+            foreach(var result in list) 
+            {
+                Console.WriteLine(result.ProductID + " " + result.UserID + " " + result.Rating + " " +result.Review+" "+result.IsLike);
+            }
+            Console.WriteLine("-------------------------------------");
+            // var res = list.Where(x => x.ProductID<2&&x.Rating<2);
+            var res =list.OrderBy(x => x.IsLike=true);
+            foreach(var result in res) 
+            {
+                Console.WriteLine(result.ProductID+" "+result.UserID+" "+result.Rating+" "+result.IsLike);
+            }
+
+           
+        }
+
+       
     }
 }
